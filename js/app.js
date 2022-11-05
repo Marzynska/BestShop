4662e4c5cde15667b2f08d0f7595c7d09f156e36
Marzynska <karolina@ekp.com.pl>
2022-11-05 17:11:56 +0100

    JS calculator finished

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 // form
-const inputProductsQty = document.querySelector("#products");
-const inputOrdersQty = document.querySelector("#orders");
+const inputProducts = document.querySelector("#products");
+const inputOrders = document.querySelector("#orders");
 const selectPackage = document.querySelector("#package");
 const dropdown = document.querySelector(".select__dropdown");
 const inputText = document.querySelector(".select__input");
-
 const accounting = document.querySelector("#accounting");
 const terminal = document.querySelector("#terminal");
 // summary
-
 const listItem = document.querySelectorAll(".list__item");
 const total = document.querySelector(".summary__total");
+const itemCalc = document.querySelectorAll(".item__calc");
+const itemPrice = document.querySelectorAll(".item__price");
+let totalPrice = document.querySelector(".total__price");
 
-
-prices = {
+const prices = {
     products: 0.5,
     orders: 0.25,
     package: {
@@ -25,28 +25,68 @@ prices = {
     terminal: 5
 };
 
+const calculate = function () {
+    let show = document.querySelectorAll(".open");
+    if (show.length > 0 ) {
+        console.log("hurra");
+        let accountingPrice;
+        accounting.checked ? accountingPrice = prices.accounting : accountingPrice = 0;
+        let terminalPrice;
+        terminal.checked ? terminalPrice = prices.terminal : terminalPrice = 0;
+        let toBePaid = inputProducts.value * prices.products + inputOrders.value * prices.orders + selectPrice + accountingPrice + terminalPrice;
+        console.log(toBePaid)
+        totalPrice.innerText = `$ ${toBePaid}`
+    } else {
+        totalPrice.innerText = `$ 0`;
+    }
+}
 
-inputProductsQty.addEventListener("input", function (event){
+inputProducts.addEventListener("input", function (){
     listItem[0].classList.add("open");
     total.classList.add("open");
+    itemCalc[0].innerText = `${inputProducts.value} * $ ${prices.products}`;
+    itemPrice[0].innerText = `$ ${inputProducts.value * prices.products}`;
+    calculate();
 });
-inputOrdersQty.addEventListener("input", function (event){
+
+inputOrders.addEventListener("input", function (){
     listItem[1].classList.add("open");
     total.classList.add("open");
+    itemCalc[1].innerText = `${inputOrders.value} * $ ${prices.orders}`;
+    itemPrice[1].innerText = `$ ${inputOrders.value * prices.orders}`;
+    calculate();
 });
-selectPackage.addEventListener("click", function (event){
+selectPackage.addEventListener("click", function (){
     selectPackage.classList.toggle("open");
 });
+let selectPrice = 0;
 dropdown.addEventListener("click", function (event) {
-    inputText.innerText=event.target.dataset.value;
+    let choice;
+    choice = event.target.dataset.value;
+    selectPackage.dataset.value = choice;
+    inputText.innerText=choice;
     listItem[2].classList.add("open");
     total.classList.add("open");
-})
-accounting.addEventListener("click", function (event){
-    listItem[3].classList.add("open");
-    total.classList.add("open");
+    itemCalc[2].innerText = choice;
+    if (choice === 'basic') {
+        selectPrice = prices.package.basic;
+    } else if (choice === 'professional') {
+        selectPrice = prices.package.professional;
+    } else {
+        selectPrice = prices.package.premium;
+    }
+    itemPrice[2].innerText = `$ ${selectPrice}`;
+    calculate();
 });
-terminal.addEventListener("click", function (event){
-    listItem[4].classList.add("open");
+accounting.addEventListener("click", function (){
+    listItem[3].classList.toggle("open");
     total.classList.add("open");
+    itemPrice[3].innerText = `$ ${prices.accounting}`;
+    calculate();
+});
+terminal.addEventListener("click", function (){
+    listItem[4].classList.toggle("open");
+    total.classList.add("open");
+    itemPrice[4].innerText = `$ ${prices.terminal}`;
+    calculate();
 });
